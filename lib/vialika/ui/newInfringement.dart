7bc2a2a443d2db9fa96d0063bfba2965a4a4f8a2
Álvaro Rubén Hurtado Maldonado <a.hurtado.bo@gmail.com>
2023-03-11 20:12:01 -0400

    Restored ffmpeg metadata extraction

--- lib/vialika/ui/newInfringement.dart
+++ lib/vialika/ui/newInfringement.dart
@@ -1,7 +1,9 @@
+import 'package:ffmpeg_kit_flutter/ffmpeg_kit_config.dart';
 import 'package:flutter/material.dart';
+import 'package:vialika_admin/drawer.dart';
 import 'package:cross_file/cross_file.dart';
 import 'package:desktop_drop/desktop_drop.dart';
-import 'package:vialika_admin/drawer.dart';
+import 'package:ffmpeg_kit_flutter/ffprobe_kit.dart';
 
 class ExampleDragTarget extends StatefulWidget {
   const ExampleDragTarget({Key? key}) : super(key: key);
@@ -11,7 +13,7 @@ class ExampleDragTarget extends StatefulWidget {
 }
 
 class _ExampleDragTargetState extends State<ExampleDragTarget> {
-  final List<XFile> _list = [];
+  XFile? currentFile;
 
   bool _dragging = false;
 
@@ -23,23 +25,44 @@ class _ExampleDragTargetState extends State<ExampleDragTarget> {
       body: Center(
         child: DropTarget(
           onDragDone: (detail) {
-            print("done");
+            currentFile = detail.files[0];
             for (XFile myFile in detail.files){
-              print(myFile.path);
+              print("Received file ${currentFile!.path}");
             }
+
+            FFprobeKit.getMediaInformation(currentFile!.path).then((session) async {
+              final information = await session.getMediaInformation();
+              print("Media information $information");
+              if (information == null) {
+                // CHECK THE FOLLOWING ATTRIBUTES ON ERROR
+                final state = FFmpegKitConfig.sessionStateToString(await session.getState());
+                final returnCode = await session.getReturnCode();
+                final failStackTrace = await session.getFailStackTrace();
+                final duration = await session.getDuration();
+                final output = await session.getOutput();
+                print("File data is $state, $returnCode, $failStackTrace, $duration, $output");
+              }else{
+                print("Size ${information.getSize()}");
+                print("Duration ${information.getDuration()}");
+                print("Format ${information.getFormat()}");
+                print("Long format ${information.getLongFormat()}");
+                print("Tags ${information.getTags()}");
+                print("H ${information.getProperty("height")}");
+                print("H ${information.getProperty("coded_height")}");
+                print("W ${information.getProperty("coded_width")}");
+                print("All ${information.getAllProperties()}");
+              }
+            });
             setState(() {
-              _list.addAll(detail.files);
+              
             });
           },
           onDragEntered: (detail) {
-            print("entered");
             setState(() {
               _dragging = true;
             });
           },
           onDragExited: (detail) {
-            print("exited");
-            
             setState(() {
               _dragging = false;
             });
@@ -48,9 +71,9 @@ class _ExampleDragTargetState extends State<ExampleDragTarget> {
             height: 200,
             width: 200,
             color: _dragging ? Colors.blue.withOpacity(0.4) : Colors.black26,
-            child: _list.isEmpty
-                ? const Center(child: Text("Drop here"))
-                : Text(_list.map((e) => e.name).join("\n")),
+            child: currentFile==null
+                ? const Center(child: Text("Suelte archivos aquí"))
+                : Text(currentFile!.name),
           ),
         ),
       ),
